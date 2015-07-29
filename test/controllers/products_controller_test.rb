d46e2d0e3427ac30b213fdcaa2382526c5c67a7d
pcuong <pcuongnn@gmail.com>
2015-07-29 17:32:48 +0700

    29/7

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,7 +14,7 @@ class ProductsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:products)
+    assert_not_nil assigns(:products)    
   end
 
   test "should get new" do
