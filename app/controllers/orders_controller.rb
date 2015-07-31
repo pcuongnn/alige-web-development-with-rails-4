015ee708172747abc58dcb79115f78e58a10b572
pcuong <pcuongnn@gmail.com>
2015-07-31 13:24:16 +0700

    31/7 chap13

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,7 +35,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-
+        OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: 'Thanks for your order!'}
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,18 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "domain.of.sender.net",
+      authentication: "plain",
+
+      ##Nhap dung dia chi email va password
+      user_name: "..@gmail.com",
+      password: "...",
+      enable_starttls_auto: true
+    }
+  end
 end
